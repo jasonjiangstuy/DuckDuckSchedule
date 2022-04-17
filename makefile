42582c03a90803a7d07d6307045915590eadde03
“jasonjiangstuy” <jasonjiangny@gmail.com>
2022-04-17 02:39:46 -0400

    working hand detection

--- makefile
+++ makefile
@@ -1,6 +1,7 @@
-all:
+all: clean
+	-gunicorn -b 0.0.0.0:3000 --workers 4 --threads 10 main:app
+
+clean:
 	-rm -r temp_web_images/*.jpeg
 	-rm -r temp_head_images/*.jpeg
 	-rm -r temp_palm_images/*.jpeg
-	-gunicorn -b 0.0.0.0:3000 --workers 4 --threads 10 main:app
-
--- pythonhelpers/imagehelping.py
+++ pythonhelpers/imagehelping.py
@@ -1,7 +1,8 @@
 import cv2
 import os
+import mediapipe as mp
 # Idea: The camera is going to constantly send us image frames
-# then we are going to continuously get hand and head locations data
+# then we are going to continuously get palm and head locations data
 # when button pressed send 5 facepics to api that are closest to expected pixels size
 def getImageData(filename):
     # identify face rectangles
@@ -23,9 +24,12 @@ def getImageData(filename):
             cv2.imwrite(newfilename, tempimg)
         except:
             pass
-    
-        # request api
-
+   
+#    idetify locations of palms
+    palm = detectPalmLocations(filename)
+    i = 0
+    newfilename = "temp_palm_images/"+str(newname)+ "-" + str(i)+".jpeg"
+    cv2.imwrite(newfilename, palm)
         # remove temp headfile 
         # os.remove(newfilename)
     # get data back, log into db
@@ -57,3 +61,28 @@ def detectFaceLocations(filename, num_of_faces):
     # cv2.imshow('img', img)
     # cv2.waitKey()
     return faces
+
+
+def detectPalmLocations(filename):
+    mpPalms = mp.solutions.hands
+    palms = mpPalms.Hands(static_image_mode=False,
+                        max_num_hands=2,
+                        min_detection_confidence=0.5,
+                        min_tracking_confidence=0.5)
+    mpDraw = mp.solutions.drawing_utils
+    img = cv2.imread(filename)
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = palms.process(imgRGB)
+    print(results.multi_hand_landmarks)
+    if results.multi_hand_landmarks:
+        for palmLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(palmLms.landmark):
+                #print(id,lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x *w), int(lm.y*h)
+                #if id ==0:
+                cv2.circle(img, (cx,cy), 3, (255,0,255), cv2.FILLED)
+
+            mpDraw.draw_landmarks(img, palmLms, mpPalms.HAND_CONNECTIONS)
+    # returns locations
+    return img
