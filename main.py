66a67d5c7402c2bcb98e6880e012f95314aef6e0
“jasonjiangstuy” <jasonjiangny@gmail.com>
2022-04-17 00:36:48 -0400

    Working websocket sent to server

--- main.py
+++ main.py
@@ -1,39 +1,57 @@
 from flask import Flask, render_template, session, request
 from flask_sock import Sock
- 
+from binascii import a2b_base64
+
 app = Flask(__name__, static_folder='./static', template_folder='./templates')
 app.config.update(TEMPLATES_AUTO_RELOAD=True)
 sock = Sock(app)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template(
-    'index.html'
-  )
+        'index.html'
+    )
+
+
 @app.route('/scan', methods=['GET', 'POST'])
 def scanner():
     return render_template(
-    'scan.html'
-  )
+        'scan.html'
+    )
+
 
 @sock.route('/scan_websocket')
 def scan_websocket(sock):
-  while True:
-    # test websocket server
-    data = sock.receive()
-    sock.send(data)
+    while True:
+        image_data = sock.receive()
+        print(type(image_data))
+
+        print(str(image_data))
+        print("Websocket Request")
+
+        # tempbytes = imagehelper.get_as_base64
+        with open("imageToSave.jpeg", "wb") as fh:
+            import urllib
+            response = urllib.request.urlopen(image_data)
+            fh.write(response.file.read())
+        fh.close()
+        sock.send("200")
+
 
 @app.route('/personality_quiz', methods=['GET', 'POST'])
 def quiz():
     if (request.method == 'POST'):
-      
-      pass
+
+        pass
     return render_template(
-      'quiz.html'
+        'quiz.html'
     )
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='3000')
 
-#useful modules
-#picking -> method of database storage: can preserve classes
-#www.MYURL/showcase.com#isFake?=True
+# useful modules
+# picking -> method of database storage: can preserve classes
+# www.MYURL/showcase.com#isFake?=True
--- static/js/scan.js
+++ static/js/scan.js
@@ -1,17 +1,37 @@
+// Initialize Websockets
+const log = (text, color) => {
+    document.getElementById('log').innerHTML += `<span style="color: ${color}">${text}</span><br>`;
+};
+
+const socket = new WebSocket('ws://' + location.host + '/scan_websocket');
+socket.addEventListener('message', ev => {
+    log('<<< ' + ev.data, 'blue');
+});
+document.getElementById('form').onsubmit = ev => {
+    ev.preventDefault();
+    const textField = document.getElementById('text');
+    log('>>> ' + textField.value, 'red');
+    socket.send(textField.value);
+    textField.value = '';
+};
+
 let camera_button = document.querySelector("#start-camera");
 let video = document.querySelector("#video");
 let click_button = document.querySelector("#click-photo");
 let canvas = document.querySelector("#canvas");
 
-camera_button.addEventListener('click', async function() {
-   	let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
-	video.srcObject = stream;
+camera_button.addEventListener('click', async function () {
+    let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+    video.srcObject = stream;
 });
 
-click_button.addEventListener('click', function() {
-   	canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-   	let image_data_url = canvas.toDataURL('image/jpeg');
+click_button.addEventListener('click', function () {
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+    let image_data_url = canvas.toDataURL('image/jpeg');
 
-   	// data url of the image
-   	console.log(image_data_url);
+    // data url of the image
+    console.log(image_data_url);
+    socket.send(image_data_url);
+
 });
+
